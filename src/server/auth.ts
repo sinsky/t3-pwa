372a31a2bf78e506bc85d364ee4e1aa2b55d4da6
sinsky <60662802+sinsky@users.noreply.github.com>
2024-01-05 22:58:44 +0900

    chore: add github provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-
+import GithubProvider from "next-auth/providers/github";
 import { env } from "@/env";
 import { db } from "@/server/db";
 
@@ -46,6 +46,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
     /**
      * ...add more providers here.
      *
